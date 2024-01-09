b036ffa17f4e579650fa367eaf2a97ea0254cbc0
onyekaonwuama <nnyeka212@gmail.com>
2024-01-09 04:08:53 -0800

    components and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,73 @@
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import { Users } from "./pages/users/Users";
+import { Products } from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
+import "./styles/global.scss";
+
 function App() {
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route path="/" element={<Home />}>
+  //       {/* <Route index element={<Home />} /> */}
+  //       <Route path="users" element={<Users />} />
+  //       <Route path="products" element={<Products />} />
+  //     </Route>
+  //   )
+  // );
+  const Layout = () => {
+    return (
+      <div className="main" >
+        <Navbar/>
+        <div className="container">
+          <div className="menuContainer">
+            <Menu/>
+          </div>
+          <div className="contentContainer">
+            <Outlet/>
+          </div>
+        </div>
+        <Footer/> 
+      </div>
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path:"/users",
+          element:<Users/>
+        },
+        {
+          path:"/products",
+          element:<Products/>
+        }
+      ]
+    },
+    {
+      path:"/login",
+      element:<Login/>
+    }
+  ]);
 
   return (
-    <div>Hello World</div>
-  )
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+import "./home.scss";
+
+export const Home = () => {
+  return (
+    <div className="home">Homes</div>
+  )
+}
+
+export default Home
