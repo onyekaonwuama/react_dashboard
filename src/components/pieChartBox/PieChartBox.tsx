92f00fd51da645e420a3d79a4999d24a9468afc5
onyekaonwuama <nnyeka212@gmail.com>
2024-01-10 03:43:58 -0800

    added barcharts

---
+++ src/components/pieChartBox/PieChartBox.tsx
@@ -0,0 +1,10 @@
+import "./pieChart.scss";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+
+const PieChartBox = () => {
+  return (
+    <div>PieChartBox</div>
+  )
+}
+
+export default PieChartBox
